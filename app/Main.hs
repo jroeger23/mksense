984c08f0fb1f7d03312018a2c06407f4282b9f47
Jonas Röger <jonas.roeger@tu-dortmund.de>
2019-12-10 01:54:39 +0100

    main - simple cmd handling

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,20 @@
 module Main where
 
+import Data.List
 import Control.Monad
-import Mksense.Parser.Logic
+import Mksense.Parser.Logic as P
+import Mksense.Logic.Formula as F
+import System.Environment
 
 main :: IO ()
-main = forever $ do
-  putStr "> "
-  a <- getLine
-  print $ run a
+main = do
+  --a <- getLine
+  args <- getArgs
+  case args of
+    "nnf":expr   -> print $ nnf $ P.run $ intercalate " " expr
+    "dnf":expr   -> print $ dnf . nnf . orand $ P.run $ intercalate " " expr
+    "knf":expr   -> print $ knf . nnf . orand $ P.run $ intercalate " " expr
+    "orand":expr -> print $ orand $ P.run $ intercalate " " expr
+    "knfcl":expr -> print $ minifyKnfClauses . knfClauses . knf . nnf . orand $ P.run $ intercalate " " expr
+    "dnfcl":expr -> print $ minifyDnfClauses . dnfClauses . dnf . nnf . orand $ P.run $ intercalate " " expr
+    expr         -> print $ P.run $ intercalate " " expr
