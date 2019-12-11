84db32e9bf9183921f7aade24034532535831244
Jonas Röger <jonas.roeger@tu-dortmund.de>
2019-12-11 19:55:41 +0100

    main - simple argument + formula parsing

--- app/Main.hs
+++ app/Main.hs
@@ -2,19 +2,14 @@ module Main where
 
 import Data.List
 import Control.Monad
+import Mksense.Cmd
 import Mksense.Parser.Logic as P
+import Mksense.Parser.Cmd as C
 import Mksense.Logic.Formula as F
 import System.Environment
 
 main :: IO ()
 main = do
-  --a <- getLine
-  args <- getArgs
-  case args of
-    "nnf":expr   -> print $ nnf $ P.run $ intercalate " " expr
-    "dnf":expr   -> print $ dnf . nnf . orand $ P.run $ intercalate " " expr
-    "knf":expr   -> print $ knf . nnf . orand $ P.run $ intercalate " " expr
-    "orand":expr -> print $ orand $ P.run $ intercalate " " expr
-    "knfcl":expr -> print $ minifyKnfClauses . knfClauses . knf . nnf . orand $ P.run $ intercalate " " expr
-    "dnfcl":expr -> print $ minifyDnfClauses . dnfClauses . dnf . nnf . orand $ P.run $ intercalate " " expr
-    expr         -> print $ P.run $ intercalate " " expr
+  a <- getLine
+  handle (opts a) (P.run . rest . opts $ a)
+  where opts = C.run
