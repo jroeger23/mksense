711864745d1ab9c6632a2142f8a6360447971733
Jonas Röger <jonas.roeger@tu-dortmund.de>
2019-12-11 19:18:07 +0100

    cmd - implemented handle function

--- src/Mksense/Cmd.hs
+++ src/Mksense/Cmd.hs
@@ -1,6 +1,7 @@
 module Mksense.Cmd where
 
 import Mksense.Logic.Data
+import Mksense.Logic.Formula as F
 
 data OutputScheme = Brief | Human deriving (Show)
 data Options = Options
@@ -17,5 +18,15 @@ data Options = Options
 defaultOptions :: Options
 defaultOptions = Options False False False False False Brief ""
 
-handle :: Options -> Expression -> Expression
-handle _ = id
+handle :: Options -> Expression -> IO ()
+handle o = do if Mksense.Cmd.nnf o        then print . F.nnf
+              else if Mksense.Cmd.orand o then print . F.orand
+              else if Mksense.Cmd.dnf o
+                      then if Mksense.Cmd.clauses o
+                              then print . F.minifyDnfClauses . F.dnfClauses . F.dnf . F.nnf . F.orand
+                              else print . F.dnf . F.nnf . F.orand
+              else if Mksense.Cmd.knf o
+                      then if Mksense.Cmd.clauses o
+                              then print . F.minifyKnfClauses . F.knfClauses . F.knf . F.nnf . F.orand
+                              else print . F.knf . F.nnf . F.orand
+              else print
