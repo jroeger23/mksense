bae6282450481aa02eedc731b6944e952ffd8a7d
Jonas Röger <jonas.roeger@tu-dortmund.de>
2019-12-12 22:01:24 +0100

    cmd - replace with qualified

--- src/Mksense/Cmd.hs
+++ src/Mksense/Cmd.hs
@@ -1,10 +1,9 @@
 module Mksense.Cmd  where
 
-import Mksense.Cmd as T
 import Mksense.Logic.Data
-import Mksense.Logic.Formula as F
+import qualified Mksense.Logic.Formula as F
 
-data OutputScheme = Brief | Human deriving (Show)
+data OutputScheme = Brief | Human
 data Options = Options
   {
     nnf :: Bool,
@@ -17,26 +16,26 @@ data Options = Options
   }
 
 instance Show Options where
-  show o = (if T.nnf o then "nnf"
-    else if T.dnf o    then "dnf"
-    else if T.knf o    then "knf"
-    else if T.orand o  then "orand"
+  show o = (if nnf o then "nnf"
+    else if dnf o    then "dnf"
+    else if knf o    then "knf"
+    else if orand o  then "orand"
     else "show")
-    ++ if T.clauses o then " clauses" else ""
+    ++ if clauses o then " clauses" else ""
 
 
 defaultOptions :: Options
 defaultOptions = Options False False False False False Brief ""
 
 handle :: Options -> Expression -> IO ()
-handle o = do if T.nnf o        then print . F.nnf
-              else if T.orand o then print . F.orand
-              else if T.dnf o
-                      then if T.clauses o
+handle o = do if nnf o        then print . F.nnf
+              else if orand o then print . F.orand
+              else if dnf o
+                      then if clauses o
                               then print . F.minifyDnfClauses . F.dnfClauses . F.dnf . F.nnf . F.orand
                               else print . F.dnf . F.nnf . F.orand
-              else if T.knf o
-                      then if T.clauses o
+              else if knf o
+                      then if clauses o
                               then print . F.minifyKnfClauses . F.knfClauses . F.knf . F.nnf . F.orand
                               else print . F.knf . F.nnf . F.orand
               else print
