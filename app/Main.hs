d8e388fd496c9c34a82a888fd9cb6c2da625cdf7
Jonas Röger <jonas.roeger@tu-dortmund.de>
2019-12-12 21:52:33 +0100

    main - removed unused imports

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,9 @@
 module Main where
 
-import Data.List
 import Control.Monad
 import Mksense.Cmd
 import Mksense.Parser.Logic as L
 import Mksense.Parser.Cmd as C
-import Mksense.Logic.Formula as F
 import System.Environment
 import System.IO
 
